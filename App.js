c8e221433a95be2a4cb81abdd28605dbfc4428ae
Mikko Kujala <m.kujala@live.com>
2024-08-28 14:19:02 +0300

    lala

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [lowerBound, setLowerBound] = useState(0)
+  const [upperBound, setUpperBound] = useState(0)
+  const [age, setAge] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Age</Text>
+      <TextInput placeholder='Input age here...'
+                 style={styles.input}
+                 onChangeText={(val) => setAge(val)}>
+      </TextInput>
+      <Text style={styles.text}>Limits</Text>
+      <Text style={styles.text}>{lowerBound}-{upperBound}</Text>
+      <View style={styles.button}>
+        <Button onPress={() => {
+            setLowerBound((220-age) * 0.65);
+            setUpperBound((220-age) * 0.85);
+          }}
+          title="Calculate"
+          style={styles.button}></Button>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    paddingLeft: 30,
+    paddingTop: 50,
+},
+  input: {
+    fontSize: 22,
   },
+  text: {
+    fontSize: 18,
+  },
+  button: {
+    padding: 5,
+    height: 200,
+    width: 300,
+  }
 });
